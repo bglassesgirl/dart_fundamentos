cc4216d560a86a27af5344f95ede07554ec4beba
Luana Braga <luanabragagomes9@gmail.com>
2023-11-14 15:00:31 -0300

    feat: add modifiers class

---
+++ lib/2_modifierss/1_modificadores.dart
@@ -0,0 +1,20 @@
+void main(){
+  String nomeCompletob= 'Luana Braga';
+  print(nomeCompletob);
+  String nomeCompletoc = 'Luana Braga Gomes';
+  print(nomeCompletoc);
+
+  // final
+  // nao pode ocorrer alteração depois de inicializadas(imutaveis_)
+  //são definida do programa em tempo de execução (runtime)
+  final nomeCompletoFinal= 'Luana BRG';
+
+  // const
+  //nao pode ser alteradas depois da inicialização
+  //são definidas no programa em tempo de COMPILAÇÃO
+  //nao podem receber valores de outras variaveis a nao ser
+  //que essas vars sao const tbm
+  const nomeCompletoConst= 'luana.brg';
+
+
+}
