a05fbee88ee9376d5a901a48ac8b9ea87d222902
Luana Braga <luanabragagomes9@gmail.com>
2023-11-14 13:45:38 -0300

    study feat: variable class

---
+++ lib/1_variables/1_variables.dart
@@ -0,0 +1,25 @@
+void main(){
+
+  int idade = 20;
+  double valorIphone = 12.000;
+
+  // pai do int e double - cabe os dois tipos
+  num qualquerNumero = 1;
+  num qualquerNumero2 = 1.1;
+
+  //var define o tipo de variavel pelo que vem depois
+  var vOne = 1;
+  var vTwo = 'Aprendendo a sintaxe de Dart';
+
+  String nomeCurso = 'Academia do Fluter';
+
+
+  // EVITAR A UTILIZAÇÃO DESSES
+  // todo mundo é filho objetct - pode receber qualquer coisa
+  Object objetoQualquer = 1.2;
+
+  //pode colocar qualquer coisa
+  dynamic dynamicQualquer = 1;
+  dynamic dynamicExemple = 'glassesgirl';
+
+}
