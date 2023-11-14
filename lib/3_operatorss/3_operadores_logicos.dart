75b83bd2693162d5d2e32014e7aec6fef2d5377c
Luana Braga <luanabragagomes9@gmail.com>
2023-11-14 16:43:51 -0300

    feat: logical operators

---
+++ lib/3_operatorss/3_operadores_logicos.dart
@@ -0,0 +1,27 @@
+void main(){
+
+    final sexo = 'M';
+    final idade = '18';
+
+    // operador E (&&)
+    if( sexo == 'M' && idade >= 18  && sexo != 'F'){
+        print('PODE ENTAR');
+    }else{
+      print('nao pode entrar');
+    }
+
+    // operador OU (||)
+    if (sexo == 'M' || idade > 18) {
+      print('pode entrar');
+    }else{
+      print('NAO pode entrar');
+    }
+
+    // invetendo a logica, ! transforma em NÃO é alguma coisa
+    if(!(sexo == 'M' && idade >= 18)) {
+       print('pode entrar');
+    }else{
+      print('NAO pode entrar');
+    }
+
+}
