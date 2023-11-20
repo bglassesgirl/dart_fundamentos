ca8f8f2c05cc374a8f5ab164564510cec0a95dbe
Luana Braga <luanabragagomes9@gmail.com>
2023-11-20 19:21:07 -0300

    feat: memory

---
+++ lib/2_dart_funcoes_colecoes/3_memory/1_memory.dart
@@ -0,0 +1,34 @@
+void main(){
+//  NUNCA ALTERAR DIRETAMENTE A LISTA
+// SEMRPE CRIAR UMA LISTA COPIA
+
+  var lista = ['luana'];
+  print(lista.hashCode);
+  funcao(lista);
+  print(lista);
+
+  var nome = 'Rodrigo Rahman';
+  print(nome);
+  print(nome.hashCode);
+  funcaob(nome);
+  print(nome);
+  print(nome.hashCode);
+}
+
+// cria um novo endereco de memoria
+void funcaob(String nome){
+   nome += 'academia do flutter';
+   print(nome);
+   print(nome.hashCode);
+}
+// fica o mesmo endereco de memoria
+void funcao(List<String> nomes){
+   print(nomes.hashCode);
+   if (nomes.isNotEmpty) {
+    }
+}
+List<Stirng> add(List<String> nomes){
+   var novosNomes = [...nomes];
+   novosNomes.add('maria');
+   return novosNomes;
+}
