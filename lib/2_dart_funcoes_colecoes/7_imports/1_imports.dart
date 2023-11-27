021ee1adb1ec96aa5c72a12c33f8db2c08ef6909
Luana Braga <luanabragagomes9@gmail.com>
2023-11-27 16:00:49 -0300

    feat: imports

---
+++ lib/2_dart_funcoes_colecoes/7_imports/1_imports.dart
@@ -0,0 +1,7 @@
+import 'soma.dart';
+
+void main(){
+  var totalDoubles = somaDoubles(1.2, 1.5);
+  print(totalDoubles);
+
+}
---
+++ lib/2_dart_funcoes_colecoes/7_imports/soma.dart
@@ -0,0 +1,5 @@
+int somaInteiros(int numero, int numerob) => numero + numerob;
+
+double somaDoubles(double numero, double numerob){
+  return numero + numerob;
+}
